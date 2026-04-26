f25db1460f74342d2c3f6552a3566df4da31a35e
Josu31506 <josuehernandezyataco@gmail.com>
2026-04-25 22:52:19 -0500

    feat: add benchmarking scripts for external hashing and sorting

---
+++ P2/bench_hash.py
@@ -0,0 +1,29 @@
+import time
+import os
+import sys
+from external_hashing import external_hash_group_by
+
+PAGE_SIZE = 4096
+# Asegúrate de que el archivo esté en la misma carpeta o usa la ruta correcta
+HEAP_FILE = 'department_employees.bin' 
+buffers = [64, 128, 256]
+
+print(f"{'BUFFER_SIZE':<12} | {'B (págs)':<8} | {'Partic.':<7} | {'T. Fase 1':<10} | {'T. Fase 2':<10} | {'T. Total':<10} | {'I/O Total'}")
+print("-" * 87)
+
+for kb in buffers:
+    buf_size = kb * 1024
+    
+    # Bloqueamos los print internos de la función para no ensuciar la tabla
+    sys.stdout = open(os.devnull, 'w')
+    try:
+        stats = external_hash_group_by(HEAP_FILE, PAGE_SIZE, buf_size, 2)
+    finally:
+        sys.stdout = sys.__stdout__ # Restauramos la consola
+    
+    b_pages = buf_size // PAGE_SIZE
+    io_total = stats['pages_read'] + stats['pages_written']
+    
+    print(f"{kb:>3} KB      | {b_pages:>8} | {stats['partitions_created']:>7} | "
+          f"{stats['time_phase1_sec']:>10.4f} | {stats['time_phase2_sec']:>10.4f} | "
+          f"{stats['time_total_sec']:>10.4f} | {io_total:>9}")
---
+++ P2/bench_sort.py
@@ -0,0 +1,21 @@
+import time
+from external_sort import external_sort
+
+PAGE_SIZE = 4096
+HEAP_FILE = 'employees.bin'
+OUTPUT_FILE = 'sorted_employees.bin'
+buffers = [64, 128, 256]
+
+print(f"{'BUFFER_SIZE':<12} | {'B (págs)':<8} | {'Runs':<6} | {'T. Fase 1':<10} | {'T. Fase 2':<10} | {'T. Total':<10} | {'I/O Total'}")
+print("-" * 85)
+
+for kb in buffers:
+    buf_size = kb * 1024
+    stats = external_sort(HEAP_FILE, OUTPUT_FILE, PAGE_SIZE, buf_size, 5) # 5 es hire_date
+    
+    b_pages = buf_size // PAGE_SIZE
+    io_total = stats['pages_read'] + stats['pages_written']
+    
+    print(f"{kb:>3} KB      | {b_pages:>8} | {stats['runs_generated']:>6} | "
+          f"{stats['time_phase1_sec']:>10.4f} | {stats['time_phase2_sec']:>10.4f} | "
+          f"{stats['time_total_sec']:>10.4f} | {io_total:>9}")
